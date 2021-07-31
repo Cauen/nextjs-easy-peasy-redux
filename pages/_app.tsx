4ad2fde70a5a6a64ab6acae4904bd505b3a3a416
Cauen <emanuel.caue@gmail.com>
2021-07-31 17:51:13 -0300

    feat: _app and index easy peasy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createStore, action, Action, StoreProvider } from "easy-peasy";
+
+type Todo = { name: string };
+export interface StoreModel {
+  todos: Todo[];
+  addTodo: Action<StoreModel, Todo>;
+  clearTodos: Action<StoreModel>;
+}
+const store = createStore<StoreModel>({
+  todos: [{ name: "Default" }],
+  addTodo: action((state, payload) => {
+    state.todos.push(payload);
+  }),
+  clearTodos: action((state) => {
+    state.todos = [];
+  }),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider store={store}>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
-export default MyApp
+export default MyApp;
